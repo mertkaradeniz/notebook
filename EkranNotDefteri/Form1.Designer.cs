496d73b000e73de0c7f6e64e05f3f9e96d0899dd
unknown <mk@jmteknoloji.com>
2019-04-16 05:08:09 +0300

    notebook
    notebook

---
+++ EkranNotDefteri/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace EkranNotDefteri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.gösterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button5 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(233, 198);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.richTextBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bugün";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(135, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Yarın";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 275);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            this.monthCalendar1.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.monthCalendar1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bugün Günlerden ";
+            this.label1.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pazartesi";
+            this.label2.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(166, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "27.07.2018";
+            this.label3.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(166, 446);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tarih";
+            this.label4.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.label4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(169, 482);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "İleri";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.button3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(12, 482);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Geri";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.button4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(9, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Tarihe Not Düşün";
+            this.label5.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.label5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label6.Location = new System.Drawing.Point(168, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "27.07.2018";
+            this.label6.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.label6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(188, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Seçili Tarih";
+            this.label7.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.label7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Not Defteri";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gösterToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(109, 48);
+            // 
+            // gösterToolStripMenuItem
+            // 
+            this.gösterToolStripMenuItem.Name = "gösterToolStripMenuItem";
+            this.gösterToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.gösterToolStripMenuItem.Text = "Göster";
+            this.gösterToolStripMenuItem.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.gösterToolStripMenuItem.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            this.çıkışToolStripMenuItem.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.çıkışToolStripMenuItem.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(12, 511);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(232, 30);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Gelişmiş Seçenekler";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseLeave += new System.EventHandler(this.richTextBox1_MouseLeave);
+            this.button5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.monthCalendar1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(260, 553);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ekran Not Defteri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gösterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ EkranNotDefteri/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Threading;
+using System.Runtime.InteropServices;
+using formAnimation;
+using Microsoft.Win32;
+namespace EkranNotDefteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=db.accdb");
+        DateTime SeciliTarih;
+        bool Status = false;
+      
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rkApp.SetValue(Application.ProductName, Application.ExecutablePath);
+
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+
+            label6.Text = monthCalendar1.SelectionRange.Start.ToShortDateString().ToString();
+            label2.Text = CultureInfo.GetCultureInfo("tr-TR").DateTimeFormat.DayNames[(int)DateTime.Now.DayOfWeek];
+            label3.Text = DateTime.Now. ToShortDateString().ToString();
+          
+            conn.Open();
+            SeciliTarih = monthCalendar1.SelectionRange.Start;
+            richTextBox1.Text = Yukle(monthCalendar1.SelectionRange.Start.ToShortDateString());
+            if (richTextBox1.Text.Trim() == "")
+                Status = true;
+        }
+
+        void Kayit(string Not,DateTime Tarih)
+        {
+            OleDbCommand com = new OleDbCommand("insert into tblnot(Notlar,Tarih)Values(@Notlar,@Tarih)", conn);
+            com.Parameters.Add("@Notlar", OleDbType.VarChar).Value = Not;
+            com.Parameters.Add("@Tarih", OleDbType.VarChar).Value = Tarih.ToShortDateString().ToString(); 
+            com.ExecuteNonQuery();
+           
+        }
+
+        string Yukle(string Tarih)
+        {         
+            OleDbCommand com = new OleDbCommand("select * from tblnot where Tarih='"+Tarih+"'", conn);
+            com.ExecuteNonQuery();
+            OleDbDataReader oku = com.ExecuteReader();
+            string Veri = "";
+            while (oku.Read())
+            {
+                Veri = oku[1].ToString();
+            }           
+           
+            return Veri;
+        }
+
+        void Guncelle(string Not,string Tarih)
+        {
+            OleDbCommand com = new OleDbCommand("update tblnot set Notlar=@Notlar where Tarih='"+Tarih+"'", conn);
+            com.Parameters.Add("@Notlar", OleDbType.VarChar).Value = Not;       
+            com.ExecuteNonQuery();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            
+            if (Status)
+            {
+                if (richTextBox1.Text.Trim() != "")
+                    Kayit(richTextBox1.Text, SeciliTarih);
+                Status = false;
+            }
+            else
+               Guncelle(richTextBox1.Text, SeciliTarih.ToShortDateString().ToString());
+           
+            label6.Text= monthCalendar1.SelectionRange.Start.ToShortDateString();
+            SeciliTarih=monthCalendar1.SelectionRange.Start;
+
+
+            richTextBox1.Text = Yukle(monthCalendar1.SelectionRange.Start.ToShortDateString());
+            if (richTextBox1.Text.Trim() == "")
+                Status = true;
+
+        }
+       static int indis = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Status)
+            {
+                if (richTextBox1.Text.Trim() != "")
+                    Kayit(richTextBox1.Text, SeciliTarih);
+                Status = false;
+            }
+            else
+                Guncelle(richTextBox1.Text, SeciliTarih.ToShortDateString().ToString());
+            
+            indis++;
+            SeciliTarih = monthCalendar1.SelectionRange.Start.AddDays(indis);
+            label6.Text = SeciliTarih.ToShortDateString().ToString();
+            richTextBox1.Text = Yukle(SeciliTarih.ToShortDateString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Status)
+            {
+                if (richTextBox1.Text.Trim() != "")
+                    Kayit(richTextBox1.Text, SeciliTarih);
+                Status = false;
+            }
+            else
+                Guncelle(richTextBox1.Text, SeciliTarih.ToShortDateString().ToString());
+            
+            indis--;
+            SeciliTarih = monthCalendar1.SelectionRange.Start.AddDays(indis);
+            label6.Text = SeciliTarih.ToShortDateString().ToString();
+            richTextBox1.Text = Yukle(SeciliTarih.ToShortDateString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SeciliTarih = DateTime.Now;
+            label6.Text = SeciliTarih.ToShortDateString().ToString();
+            richTextBox1.Text = Yukle(SeciliTarih.ToShortDateString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SeciliTarih = DateTime.Now.AddDays(1);
+            label6.Text = SeciliTarih.ToShortDateString().ToString();
+            richTextBox1.Text = Yukle(SeciliTarih.ToShortDateString());
+        }
+
+        private void PlaceLowerRight(int value)
+        {
+            try
+            {
+                //Determine "rightmost" screen
+                Screen rightmost = Screen.AllScreens[0];
+                foreach (Screen screen in Screen.AllScreens)
+                {
+                    if (screen.WorkingArea.Right > rightmost.WorkingArea.Right)
+                        rightmost = screen;
+                }
+
+                this.Left = rightmost.WorkingArea.Right - this.Width + 10 + value;
+                this.Top = 10;
+            }
+            catch { }
+
+           
+        }
+        int kayma = 249;
+        bool ayirma = false;
+        protected override void OnLoad(EventArgs e)
+        {
+            PlaceLowerRight(kayma);
+            base.OnLoad(e);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            PlaceLowerRight(0);
+            ayirma = false;
+        }
+
+        void slep()
+        {
+            Thread.Sleep(700); 
+            if(ayirma)
+            PlaceLowerRight(kayma);       
+        }
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            Thread th = new Thread(new ThreadStart(slep));
+            th.Start();
+            ayirma = true;
+        }
+
+        void _ayirma()
+        {
+            ayirma = false;
+        }
+
+        private void monthCalendar1_MouseMove(object sender, MouseEventArgs e)
+        {
+            _ayirma();
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void richTextBox1_MouseLeave(object sender, EventArgs e)
+        {
+            ayirma = true;
+            Thread th = new Thread(new ThreadStart(slep));
+            th.Start();
+        }
+    }
+}
